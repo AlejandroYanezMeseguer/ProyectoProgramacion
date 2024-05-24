59951e15e28ebdb4ffb531e322c57072971cc9ae
aleja <alejandritoym@gmail.com>
2024-05-24 16:20:09 +0200

    clases APIs

---
+++ src/API/ConexionAPIArmas.java
@@ -0,0 +1,49 @@
+package API;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ConexionAPIArmas {
+
+    public void APIArmas() {
+
+        String apiUrl = "https://eldenring.fanapis.com/api/weapons";
+
+        try {
+
+            URL url = new URL(apiUrl);
+
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            con.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            String inputLine;
+
+            StringBuffer content = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+
+                content.append(inputLine);
+
+            }
+
+            // Procesar JSON y actualizar interfaz aquí
+
+            in.close();
+
+            con.disconnect();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
